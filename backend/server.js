20de47e13b98597e6c8c93590b93da126007d709
NICOLLY DE OLIVEIRA DO NASCIMENTO <133240077+yvnkzy@users.noreply.github.com>
2024-11-19 14:04:41 +0000

    pronto

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import express from "express";
 import { connectToDatabase } from "./config/db.js";
 import bookRouter from "./routes/book.routes.js";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3737;
 const __dirname = path.resolve();
 
 const app = express();
